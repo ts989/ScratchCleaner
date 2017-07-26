f5c69d5b060ed4accadaf9985e9b027a36978072
Tomislav Stilinovic <Tomislav.Stilinovic@srce.hr>
2017-07-26 15:08:00 +0200

    lost+found

--- scratch-cleaner.sh
+++ scratch-cleaner.sh
@@ -3,6 +3,7 @@
 tag="[isabella-mon]"
  
 du -h /scratch/*  > /scratch/for_del 
+sed -i '/lost+found/d' /scratch/for_del
 
 #provjerava datoteke oblika job-id.brojevi.slova.q 
 for f in $(ls /scratch/ | egrep [0-9]+.[0-9]+.[a-z]+.q | sed s/[.].*//g | sort -n | uniq); do 
@@ -12,7 +13,7 @@ for f in $(ls /scratch/ | egrep [0-9]+.[0-9]+.[a-z]+.q | sed s/[.].*//g | sort -
 done 
 
 #provjerava datoteke koje nisu gore navedenog oblika
-for f in $(ls /scratch/ | egrep -v [0-9]+.[0-9]+.[a-z]+.q | egrep -v lost+found); do  
+for f in $(ls /scratch/ | egrep -v [0-9]+.[0-9]+.[a-z]+.q); do  
         if [[ $(lsof | grep ${f} | wc -l) != 0 ]]; then  
                 sed -i '/'${f}'/d' /scratch/for_del  
         fi  
@@ -30,15 +31,15 @@ rm -f /scratch/1mon
 
 #salje mail/brise
 if [[ -s /scratch/for_del ]] ; then 
-        cat /scratch/for_del | mail -s "$tag Files for deletion on $(hostname)" "isabella-dezurni@srce.hr"
+        cat /scratch/for_del | mail -s "$tag Files for deletion on $(hostname)" "Tomislav.Stilinovic@srce.hr"
 	if [[ -s /scratch/rescue_me ]] ; then
-		cat /scratch/rescue_me | mail -s "$tag Files for rescuing on $(hostname)" "isabella-dezurni@srce.hr"  
+		cat /scratch/rescue_me | mail -s "$tag Files for rescuing on $(hostname)" "Tomislav.Stilinovic@srce.hr"  
 	fi
         #cat /scratch/for_del | awk '{print $2}' | xargs rm -rf  
         rm -f /scratch/for_del /scratch/rescue_me 
 else 
 	if [[ -s /scratch/rescue_me ]] ; then
-                cat /scratch/rescue_me | mail -s "$tag Files for rescuing on $(hostname)" "isabella-dezurni@srce.hr"
+                cat /scratch/rescue_me | mail -s "$tag Files for rescuing on $(hostname)" "Tomislav.Stilinovic@srce.hr"
         fi
         rm -f /scratch/for_del /scratch/rescue_me
 fi
