e96517b0a532ec47463aa507da49e927899caa85
Tomislav Stilinovic <Tomislav.Stilinovic@srce.hr>
2017-07-27 11:46:51 +0200

    Fixed some bugs

--- scratch-cleaner.sh
+++ scratch-cleaner.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 tag="[isabella-mon]"
+mail="isabella-dezurni@srce.hr"
  
 du -h /scratch/*  > /scratch/for_del 
 sed -i '/lost+found/d' /scratch/for_del
@@ -22,7 +23,7 @@ done
 #daje sansu mladjima od 30 dana
 touch -d '1 month ago' /scratch/1mon
 for f in $(cat /scratch/for_del | awk '{print substr($2,10)}'); do
-	if [ $f -nt /scratch/1mon ]; then
+	if [ /scratch/$f -nt /scratch/1mon ]; then
 		echo $(cat /scratch/for_del | grep $f) >> /scratch/rescue_me
 		sed -i '/'${f}'/d' /scratch/for_del
 	fi
@@ -31,15 +32,14 @@ rm -f /scratch/1mon
 
 #salje mail/brise
 if [[ -s /scratch/for_del ]] ; then 
-        cat /scratch/for_del | mail -s "$tag Files for deletion on $(hostname)" "Tomislav.Stilinovic@srce.hr"
+        cat /scratch/for_del | mail -s "$tag Files for deletion on $(hostname)" $mail
 	if [[ -s /scratch/rescue_me ]] ; then
-		cat /scratch/rescue_me | mail -s "$tag Files for rescuing on $(hostname)" "Tomislav.Stilinovic@srce.hr"  
+		cat /scratch/rescue_me | mail -s "$tag Files for rescuing on $(hostname)" $mail  
 	fi
-        #cat /scratch/for_del | awk '{print $2}' | xargs rm -rf  
-        rm -f /scratch/for_del /scratch/rescue_me 
+        #cat /scratch/for_del | awk '{print $2}' | xargs rm -rf   
 else 
 	if [[ -s /scratch/rescue_me ]] ; then
-                cat /scratch/rescue_me | mail -s "$tag Files for rescuing on $(hostname)" "Tomislav.Stilinovic@srce.hr"
+                cat /scratch/rescue_me | mail -s "$tag Files for rescuing on $(hostname)" $mail
         fi
-        rm -f /scratch/for_del /scratch/rescue_me
 fi
+rm -f /scratch/for_del /scratch/rescue_me
